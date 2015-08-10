59d9a4efd85c598e889ddda01d148ff56ff49b4c
alain <ninsuo@gmail.com>
2015-08-10 10:51:19 +0200

    backup commit

--- Services/Environment.php
+++ Services/Environment.php
@@ -2,13 +2,20 @@
 
 namespace Fuz\GenyBundle\Services;
 
+use Symfony\Component\HttpKernel\Config\FileLocator;
+use Symfony\Component\Serializer\SerializerInterface;
+
 class Environment
 {
 
+    protected $locator;
+    protected $serializer;
     protected $types;
 
-    public function __construct()
+    public function __construct(FileLocator $locator, SerializerInterface $serializer)
     {
+        $this->locator = $locator;
+        $this->serializer = $serializer;
         $this->types = array();
     }
 
@@ -17,6 +24,13 @@ class Environment
         if (array_key_exists($path, $this->types)) {
             return $this->types[$path];
         }
+
+        $realpath = $this->locator->locate($path);
+        $contents = file_get_contents($realpath);
+        $extension = strtolower(pathinfo($realpath, PATHINFO_EXTENSION));
+
+        $array = $this->serializer->deserialize($contents, null, $extension);
+        \Symfony\Component\VarDumper\VarDumper::dump($array);
 
 
         /*
