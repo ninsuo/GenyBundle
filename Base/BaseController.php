48b4f2c53afa3b5923737319521e26ce0824b83f
Alain Tiemblo <alain.tiemblo@blablacar.com>
2015-08-27 18:27:07 +0200

    small tests

---
+++ Base/BaseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Fuz\GenyBundle\Base;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BaseController extends Controller
+{
+
+    public function info($message, array $parameters = array())
+    {
+        $this->addFlash('info', $this->trans($message, $parameters));
+    }
+
+    public function alert($message, array $parameters = array())
+    {
+        $this->addFlash('alert', $this->trans($message, $parameters));
+    }
+
+    public function danger($message, array $parameters = array())
+    {
+        $this->addFlash('danger', $this->trans($message, $parameters));
+    }
+
+    public function success($message, array $parameters = array())
+    {
+        $this->addFlash('success', $this->trans($message, $parameters));
+    }
+
+    public function trans($property, array $parameters = array())
+    {
+        return $this->container->get('translator')->trans($property, $parameters);
+    }
+
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Fuz\GenyBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Fuz\GenyBundle\Base\BaseController;
+
+class DefaultController extends BaseController
+{
+
+    /**
+     * @Route("/bob", name="xxx")
+     * @Template()
+     */
+    public function indexAction(Request $request)
+    {
+    }
+
+}
