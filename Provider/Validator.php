c455745246c1f024f1b49313a3d93cf4b908a1ee
Alain <ninsuo@gmail.com>
2015-11-13 23:06:04 +0100

    implemented events on validator

--- Provider/Validator.php
+++ Provider/Validator.php
@@ -3,6 +3,7 @@
 namespace Fuz\GenyBundle\Provider;
 
 use Fuz\GenyBundle\Base\BaseService;
+use Fuz\GenyBundle\Event\GenyEvent;
 use Fuz\GenyBundle\Exception\ValidatorException;
 use Fuz\GenyBundle\Data\Resources\ResourceInterface;
 use Fuz\GenyBundle\Provider\Validator\ValidatorInterface;
@@ -13,9 +14,14 @@ class Validator extends BaseService
 
     public function boot(ResourceInterface $resource)
     {
+        $event = new GenyEvent($resource);
+        $dispatcher = $this->get('event_dispatcher');
+
         if (is_null($resource->getNormalized())) {
             throw new ValidatorException("Resource should be normalized before being validated.");
         }
+
+        $dispatcher->dispatch('geny.validator.pre_boot', $event);
 
         foreach ($this->validators as $validator) {
             if ($validator->supports($resource)) {
@@ -26,11 +32,16 @@ class Validator extends BaseService
             }
         }
 
+        $dispatcher->dispatch('geny.validator.post_boot', $event);
+
         throw new ValidatorException(sprintf("No validator found for class '%s'.", get_class($resource)));
     }
 
     public function validate(ResourceInterface $resource)
     {
+        $event = new GenyEvent($resource);
+        $dispatcher = $this->get('event_dispatcher');
+
 //        if (is_null($resource->getType())) {
 //            throw new ValidatorException("Type should be built before being validated.");
 //        }
@@ -39,11 +50,15 @@ class Validator extends BaseService
             $this->boot($resource);
         }
 
+        $dispatcher->dispatch('geny.validator.pre_validate', $event);
+
         foreach ($this->validators as $validator) {
             if ($validator->supports($resource)) {
                 return $validator->validate($resource);
             }
         }
+
+        $dispatcher->dispatch('geny.validator.post_validate', $event);
 
         throw new ValidatorException(sprintf("No validator found for class '%s'.", get_class($resource)));
     }
