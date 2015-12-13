9447b1741cad44dce388e93417bbc96186229bf3
alain tiemblo <ninsuo@gmail.com>
2015-12-13 13:01:24 +0100

    backup commit

---
+++ Resources/public/js/jquery.geny.js
@@ -0,0 +1,19 @@
+;(function( $ ) {
+
+    $.geny = (function ($) {
+
+        var disableFields = function (formId) {
+
+
+        }; // disableFields
+
+        return {
+            disableFields: disableFields,
+        };
+
+    })( $ ); // $.symfony2
+
+
+    // put event listeners here
+
+})( jQuery );
