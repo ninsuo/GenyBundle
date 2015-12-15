0f6f2ac18a12c93696bf99cad18159e80863e0b3
alain tiemblo <ninsuo@gmail.com>
2015-12-16 00:48:21 +0100

    improved ui

--- Resources/public/js/jquery.geny.js
+++ Resources/public/js/jquery.geny.js
@@ -27,20 +27,31 @@
 
     body.off('keyup keypress', '.geny-script-no-submit');
     body.on('keyup keypress', '.geny-script-no-submit', function (e) {
-        if (e.which === 13) {
+        if (13 === e.which) {
             e.preventDefault();
             return false;
         }
     });
 
-    body.off('click', '.geny-script-field-toggle');
-    body.on('click', '.geny-script-field-toggle', function (e) {
-        $('.geny-script-field-handle').addClass('hide');
-        $('.geny-script-field-settings').removeClass('hide');
+    body.off('click', '.geny-script-display-button');
+    body.on('click', '.geny-script-display-button', function (e) {
+        var target = $(this).data('geny-target');
+        $(target).trigger('click');
+    });
 
+    body.off('click', '.geny-script-display-toggle');
+    body.on('click', '.geny-script-display-toggle', function (e) {
+        $('.geny-script-display-handle').addClass('hide');
+        $('.geny-script-display-settings').removeClass('hide');
         var field_id = $(this).data('geny-field');
-        $('#geny-field-details-' + field_id).removeClass('hide');
-        $('#geny-field-settings-' + field_id).addClass('hide');
+        if ('#' === field_id) {
+            var form_id = $(this).data('geny-form');
+            $('#geny-form-' + form_id).removeClass('hide');
+            $('#geny-settings-' + form_id).addClass('hide');
+        } else {
+            $('#geny-field-details-' + field_id).removeClass('hide');
+            $('#geny-field-settings-' + field_id).addClass('hide');
+        }
     });
 
 })( jQuery );
