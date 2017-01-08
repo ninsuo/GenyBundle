7cd556b01632c4b1e3a5cc08198aa5c54cfb42e6
Gilles Lengy <gilles.lengy@gmail.com>
2017-01-08 14:15:15 +0100

    FormType with a save button

--- Form/FormType.php
+++ Form/FormType.php
@@ -5,27 +5,27 @@ namespace GenyBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class FormType extends AbstractType
-{
+class FormType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('submit')
+                ->add('title')
+                ->add('description')
+                ->add('submit')
+                ->add('save', SubmitType::class)
         ;
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'GenyBundle\Entity\Form'
         ));
@@ -34,8 +34,8 @@ class FormType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'genybundle_form';
     }
+
 }
