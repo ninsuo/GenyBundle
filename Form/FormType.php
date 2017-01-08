0f1c1a7d2a11c9ad5651b2308807f9657fcd4e8b
Gilles Lengy <gilles.lengy@gmail.com>
2017-01-08 13:52:36 +0100

    FormType generated in command line

---
+++ Form/FormType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace GenyBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FormType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GenyBundle\Entity\Form'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'genybundle_form';
+    }
+}
