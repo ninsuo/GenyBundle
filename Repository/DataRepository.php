e78fa6b6fd6d444ebafd50533df368030433451b
Gilles Lengy <gilles.lengy@artaban.fr>
2017-01-12 11:20:19 +0100

    Function dataSet from DataRepository return an array with field ID, Field Label and DataText text

--- Repository/DataRepository.php
+++ Repository/DataRepository.php
@@ -10,10 +10,8 @@ use Doctrine\ORM\Query;
 class DataRepository extends \Doctrine\ORM\EntityRepository {
 
     public function dataSet($set_id) {
-
-        //$results = $this->_em->createQuery('SELECT d, f FROM GenyBundle:Data d LEFT JOIN d.field_id f WHERE d.set_id = :set_id')
-        //$results = $this->_em->createQuery('SELECT IDENTITY(d.field_id), f.label, dt.data_id FROM GenyBundle:Data d, GenyBundle:DataText dt LEFT JOIN d.field_id f WHERE d.set_id = :set_id AND d.id = dt.data_id') 
-        $results = $this->_em->createQuery('SELECT IDENTITY(d.field_id), f.label FROM GenyBundle:Data d LEFT JOIN d.field_id f WHERE d.set_id = :set_id')
+ 
+        $results = $this->_em->createQuery('SELECT IDENTITY(d.field_id), f.label, dt.text FROM GenyBundle:Data d LEFT JOIN d.field_id f LEFT JOIN d.data_text_id dt WHERE d.set_id = :set_id')
                 ->setParameter('set_id', $set_id)
                 ->getResult();
 
