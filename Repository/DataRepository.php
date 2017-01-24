11a88b5514046b3840d11c86e7cc204cb50855cf
Gilles Lengy <gilles.lengy@artaban.fr>
2017-01-24 11:12:06 +0100

    DataRepository with a new function : dataField that gets some info about the data and the field associated

--- Repository/DataRepository.php
+++ Repository/DataRepository.php
@@ -9,8 +9,31 @@ use Doctrine\ORM\Query;
  */
 class DataRepository extends \Doctrine\ORM\EntityRepository {
 
+    public function dataField($id) {
+
+        $data = $this->_em->getRepository('GenyBundle:Data')
+                ->findOneById($id);
+
+        $form = $data->getForm();
+
+        $data_data = $data->getData();
+
+        $data_field = array();
+        $i = 0;
+        while (list($key, $value) = each($data_data)) {
+            $field = $this->_em->getRepository('GenyBundle:Field')->findOneBy(array('name' => $key, 'form' => $form));
+            $label = $field->getLabel();
+            $data_field[$i]['value'] = $value;
+            $data_field[$i]['field_name'] = $value;
+            $data_field[$i]['field_label'] = $label;
+            $i++;
+        }
+
+        return $data_field;
+    }
+
     public function dataSet($set_id) {
- 
+
         $results = $this->_em->createQuery('SELECT IDENTITY(d.field_id), IDENTITY(d.data_text_id) as data_text_id, f.name, f.label, dt.text FROM GenyBundle:Data d LEFT JOIN d.field_id f LEFT JOIN d.data_text_id dt WHERE d.set_id = :set_id')
                 ->setParameter('set_id', $set_id)
                 ->getResult();
@@ -18,5 +41,5 @@ class DataRepository extends \Doctrine\ORM\EntityRepository {
 
         return $results;
     }
-    
+
 }
